a31b459f01a544c0ef4f53f4c0c9616d889f4167
Cris9898 <157153132+Cris9898@users.noreply.github.com>
2024-04-08 06:00:04 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,30 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let quien = ["mi perro", "mi abuela", "el mensajero", "mi pajaro"];
+  let accion = ["comio", "orino", "aplasto", "rompio"];
+  let que = ["mi tarea", "mi telefono", "mi auto"];
+  let cuando = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras me estaba ejercitando",
+    "cuando almorzaba",
+    "mientras estaba rezando"
+  ];
+  let randomquien = Math.floor(Math.random() * quien.length);
+  let randomaccion = Math.floor(Math.random() * accion.length);
+  let randomque = Math.floor(Math.random() * que.length);
+  let randomcuando = Math.floor(Math.random() * cuando.length);
+  let str = document.write(
+    "<h1>" +
+      " " +
+      quien[randomquien] +
+      " " +
+      accion[randomaccion] +
+      " " +
+      que[randomque] +
+      " " +
+      cuando[randomcuando] +
+      "<h1>"
+  );
 };
